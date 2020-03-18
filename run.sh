4344bb9da8d1e903c6f6f806579210d59829f578
liuliu <liuliu@hfepay.com>
2020-03-18 16:32:55 +0800

    更新脚本

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 pwd=Liu8440
 host=127.0.0.1
 port=8011
+path="`pwd`/log"
 httpaddr=http://127.0.0.1:8002/hook/on_sip_record
-./build/fsEventController -a $host -p $port -w $pwd -u $httpaddr
+./build/fsEventController -a $host -p $port -w $pwd -u $httpaddr -d $path
